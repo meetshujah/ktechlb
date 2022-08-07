afaa2e36f241bf1bb72c7bddf69cc7a5c60cc520
shuja uddin <meetshujah@gmail.com>
2022-08-07 23:24:38 +0500

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,76 @@
-//JQuery Module Pattern
-"use strict";
 
-// An object literal
-const app = {
-  init() {
-    app.functionOne();
-  },
-  functionOne() {
+ $(document).ready(function () {
+  //Items
+  var $card = $(".c-card_b");
+  var $container = $(".innercustom");
+  var $bike = $(".innercustom img");
+
+  //Moving Animation Event
+  $container.on("mousemove", function (e) {
+    let xAxis = (window.innerWidth / 2 - e.clientX) / 30;
+    let yAxis = (window.innerHeight / 2 - e.clientY) / 30;
+    $card.css("transform", `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`);
+  });
+
+  //Animate on Hover
+  $container.hover(function () {
+    $card.toggleClass("has-transform");
+    $bike.toggleClass("has-transform");
+  });
+
+  //Pop Back on mouseleave
+  $container.on("mouseleave", function () {
+    $card.css("transform", `rotateY(0deg) rotateX(0deg)`);
+  });
+
+  function theKingIsBlackPageCallback(n) {
+    return {
+      type: 'image',
+      src: 'books/image/theKingIsBlack/'+(n+1)+'.jpg',
+      interactive: false
+    };
   }
-};
 
-$("document").ready(() => {
-  app.init();
+  $('#flipBookPDF').FlipBook({
+    pageCallback: theKingIsBlackPageCallback,
+    pages: 40,
+    propertiesCallback: function(props) {
+      props.cssLayersLoader = function(n, clb) {// n - page number
+        clb([{
+          css: '.hd {margin-top: 200px;background-color: red;}',
+          html: '<h1 class="hd">CSS3 Layer - Hello</h1>',
+          js: function (jContainer) {
+            console.log(jContainer);
+            return {
+              hide: function() {console.log('hide');},
+              hidden: function() {console.log('hidden');},
+              show: function() {console.log('show');},
+              shown: function() {console.log('shown');},
+              dispose: function() {console.log('dispose');}
+            };
+          }
+        }]);
+      };
+      props.cover.color = 0x000000;
+      return props;
+    },
+    template: {
+      html: 'templates/default-book-view.html',
+      styles: [
+        'css/short-white-book-view.css'
+      ],
+      links: [
+        {
+          rel: 'stylesheet',
+          href: 'css/font-awesome.min.css'
+        }
+      ],
+      script: 'js/default-book-view.js',
+      sounds: {
+        startFlip: 'sounds/start-flip.mp3',
+        endFlip: 'sounds/end-flip.mp3'
+      }
+    }
+  });
+  $('.demo-msg').css('display','none !important');
 });
